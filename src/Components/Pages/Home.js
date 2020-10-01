34d99a785686a08995bb13c2c96102008d908067
tkardos <kardos.t84@gmail.com>
2020-10-01 20:41:13 +0200

    led button added

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -8,6 +8,7 @@ function Home() {
   const [items, setItems] = useState([]);
   const [countryCode, selectCountryCode] = useState("");
   const [countryData, setCountryData] = useState("");
+  const [ledButton, setLedButton] = useState("default");
 
   useEffect(() => {
     fetch(`${hostname.back}/api/summary`)
@@ -18,30 +19,55 @@ function Home() {
   useEffect(() => {
     let country = items.find((item) => item.CountryCode === countryCode);
     setCountryData(country);
-    console.log(items);
   }, [countryCode, items]);
 
   function mySubmit(e) {
     e.preventDefault();
-    // console.log(e.target[0].value)
     selectCountryCode(e.target[0].value);
     return;
   }
 
+  function led(e) {
+    e.preventDefault();
+    if (countryData) {
+      setLedButton('d-none');
+      try {
+        fetch(`${hostname.back}/api/country`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(
+            {
+              "recovered": countryData.TotalRecovered,
+              "confirmed": countryData.TotalConfirmed,
+              "deaths": countryData.TotalDeaths
+            })
+        })
+          .then((response) => response.json())
+          .then((json) => {            
+            console.log(json)
+            setLedButton('default');
+            return;
+          });
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
   return (
     <div>
-      <h1 className="h1-title-home mt-5"> Home page </h1>
+      <h1 className="h2 mt-5"> Covid Watch 1.0 </h1>
 
-      <Container className="w-100">
-        <div className="mb-5" style={{ padding: 100 }} />
-        <Row>
+      <Container fluid style={{ width: "90%" }}>
+        <div className="mb-5" style={{ padding: 25 }} />
+        <Row className="justify-content-md-center">
           <Col
             md={6}
-            style={{ backgroundColor: "var(--basicred)", height: "150px" }}
+            style={{ backgroundColor: "var(--basicred)", height: "150px", borderRadius: "25px" }}
           >
             <Form className="mt-3" onSubmit={mySubmit}>
               <Form.Group>
-                <Form.Label className="mb-3"> Select a country </Form.Label>
+                <Form.Label className="mb-3 h5"> Select a country </Form.Label>
                 <Row style={{ borderRadius: "25px" }}>
                   <Col md={9}>
                     <Form.Control as="select">
@@ -68,44 +94,21 @@ function Home() {
                       type="submit"
                       value="Submit"
                       className="btn btn-secondary"
+                      style={{ marginRight: "10px" }}
+
                     />
+                    <button className={'btn btn-info ' + ledButton} onClick={led}> led </button>
                   </Col>
                 </Row>
               </Form.Group>
             </Form>
           </Col>
+        </Row>
 
-          <Col md={6} style={{ backgroundColor: "lightblue" }}>
+        <Row className="mt-3 justify-content-md-center">
+          <Col md={5} style={{ backgroundColor: "lightblue", borderRadius: "25px", margin: "10px" }}>
             <div className="m-3">
               <PieChart
-                keys={
-                  countryData
-                    ? ["New recovered", "New confirmed", "New deaths"]
-                    : ""
-                }
-                values={
-                  countryData
-                    ? [
-                      countryData.NewRecovered,
-                      countryData.NewConfirmed,
-                      countryData.NewDeaths
-                    ]
-                    : []
-                }
-                name="Daily Covid Data"
-              ></PieChart>
-              <p className="mt-3">
-                {" "}
-                {countryData
-                  ? countryData.Date.replace("T", " ").replace("Z", " ")
-                  : ""}{" "}
-              </p>
-            </div>
-          </Col>
-
-          <Col md={6} style={{ backgroundColor: "lightblue" }}>
-            <div className="m-3">
-              <BarChart
                 keys={
                   countryData
                     ? ["Total recovered", "Total confirmed", "Total deaths"]
@@ -120,7 +123,35 @@ function Home() {
                     ]
                     : []
                 }
-                name="Total Covid Data by Country"
+                name="Total Covid Data"
+              ></PieChart>
+              <p className="mt-3">
+                {" "}
+                {countryData
+                  ? countryData.Date.replace("T", " ").replace("Z", " ")
+                  : ""}{" "}
+              </p>
+            </div>
+          </Col>
+
+          <Col md={5} style={{ backgroundColor: "lightblue", borderRadius: "25px", margin: "10px" }}>
+            <div className="m-3">
+              <BarChart
+                keys={
+                  countryData
+                    ? ["Daily recovered", "Daily confirmed", "Daily deaths"]
+                    : ""
+                }
+                values={
+                  countryData
+                    ? [
+                      countryData.NewRecovered,
+                      countryData.NewConfirmed,
+                      countryData.NewDeaths
+                    ]
+                    : []
+                }
+                name="Daily Covid Data"
               ></BarChart>
               <p className="mt-3">
                 {" "}
